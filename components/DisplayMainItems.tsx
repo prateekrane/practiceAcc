7f0ee82d0a914e362eae8065815dc279fada55a3
prateekrane <prateekrane7@gmail.com>
2025-12-30 13:46:53 +0530

    delete and update

--- components/DisplayMainItems.tsx
+++ components/DisplayMainItems.tsx
@@ -1,72 +1,98 @@
-import react from 'react';
-import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native';
-import { Product } from './UseEffectAxiosDemo';
+import react from "react";
+import { View, Text, StyleSheet, Image, TouchableOpacity } from "react-native";
+import { Product } from "./UseEffectAxiosDemo";
 
-export default function DisplayMainItem(props: { items: Product, getId: (num: number) => void }) {
-
-
-    return (
-        <View style={styles.container}>
-            <View style={styles.cardsty}>
-                <View>
-                    <Image source={{ uri: props.items.image }} style={styles.imgsty} />
-                </View>
-                <View style={styles.contentcontainer}>
-                    <Text style={styles.titlestyle}>{props.items.title}</Text>
-                    <Text style={styles.categorystyle}>{props.items.category}</Text>
-                    <View style={styles.priceContainer}><Text style={{ fontWeight: "600" }}>${props.items.price}</Text></View>
-                </View>
-                <TouchableOpacity style={styles.deleteButton} onPress={() => { props.getId(props.items.id) }}>
-                    <Text style={styles.deleteText}>🗑️</Text>
-                </TouchableOpacity>
-            </View>
-
+export default function DisplayMainItem(props: {
+  items: Product;
+  getId: (num: number) => void;
+  getVal: (num: number, itemPrice: number) => void;
+}) {
+  return (
+    <View style={styles.container}>
+      <View style={styles.cardsty}>
+        <View>
+          <Image source={{ uri: props.items.image }} style={styles.imgsty} />
         </View>
-    )
+        <View style={styles.contentcontainer}>
+          <Text style={styles.titlestyle}>{props.items.title}</Text>
+          <Text style={styles.categorystyle}>{props.items.category}</Text>
+          <View style={styles.priceContainer}>
+            <Text style={{ fontWeight: "600" }}>${props.items.price}</Text>
+          </View>
+        </View>
+        <TouchableOpacity
+          style={styles.editButton}
+          onPress={() => {
+            props.getVal(props.items.id, props.items.price);
+          }}
+        >
+          <Text style={styles.editText}>✏️</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.deleteButton}
+          onPress={() => {
+            props.getId(props.items.id);
+          }}
+        >
+          <Text style={styles.deleteText}>🗑️</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
 }
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        width: "100%",
-        justifyContent: "center",
-        alignItems: "center",
+  container: {
+    flex: 1,
+    width: "100%",
+    justifyContent: "center",
+    alignItems: "center",
 
-        padding: 10,
-    },
-    cardsty: {
-        height: 120,
-        width: 350,
-        flexDirection: "row",
-        backgroundColor: "white",
-        borderRadius: 15,
+    padding: 10,
+  },
+  cardsty: {
+    height: 120,
+    width: 350,
+    flexDirection: "row",
+    backgroundColor: "white",
+    borderRadius: 15,
 
-        padding: 15
-
-    },
-    imgsty: {
-        height: 90,
-        width: 80,
-        resizeMode: "contain"
-    },
-    contentcontainer: {
-        flex: 1,
-    }, titlestyle: {
-        fontSize: 12.5,
-        fontWeight: "bold"
-    },
-    categorystyle: {
-        fontWeight: "400",
-        color: "gray"
-    },
-    priceContainer: {
-        justifyContent: "flex-end", alignItems: "flex-end"
-    },
-    deleteButton: {
-        justifyContent: 'center',
-        alignItems: 'center',
-        padding: 5,
-    },
-    deleteText: {
-        fontSize: 20,
-    }
-})
+    padding: 15,
+  },
+  imgsty: {
+    height: 90,
+    width: 80,
+    resizeMode: "contain",
+  },
+  contentcontainer: {
+    flex: 1,
+  },
+  titlestyle: {
+    fontSize: 12.5,
+    fontWeight: "bold",
+  },
+  categorystyle: {
+    fontWeight: "400",
+    color: "gray",
+  },
+  priceContainer: {
+    justifyContent: "flex-end",
+    alignItems: "flex-end",
+  },
+  editButton: {
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 5,
+    marginRight: 4,
+  },
+  editText: {
+    fontSize: 18,
+  },
+  deleteButton: {
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 5,
+  },
+  deleteText: {
+    fontSize: 20,
+  },
+});
--- components/UseEffectAxiosDemo.tsx
+++ components/UseEffectAxiosDemo.tsx
@@ -1,129 +1,195 @@
-import react, { useEffect, useState } from 'react';
-import { View, Text, FlatList, StyleSheet, TouchableOpacity, ScrollView } from 'react-native';
-import axios from 'axios';
+import react, { useEffect, useState } from "react";
+import {
+  View,
+  Text,
+  FlatList,
+  StyleSheet,
+  TouchableOpacity,
+  ScrollView,
+} from "react-native";
+import axios from "axios";
 
-import DisplayMainItem from './DisplayMainItems';
+import DisplayMainItem from "./DisplayMainItems";
 
 export type Product = {
-    id: number;
-    title: string;
-    price: number;
-    image: string;
-    category: string;
+  id: number;
+  title: string;
+  price: number;
+  image: string;
+  category: string;
 };
 export default function UseEffectAxiosDemo() {
-    const [data, setData] = useState<Product[]>([]);
-    const [originalData, setOriginalData] = useState<Product[]>([]);
+  const [data, setData] = useState<Product[]>([]);
+  const [originalData, setOriginalData] = useState<Product[]>([]);
 
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const response = await axios.get("https://fakestoreapi.com/products");
-                setData(response.data);
-                setOriginalData(response.data);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get("https://fakestoreapi.com/products");
+        setData(response.data);
+        setOriginalData(response.data);
+      } catch (error) {
+        console.log("Error is:" + error);
+      }
+    };
+    fetchData();
+  }, []);
 
-            } catch (error) {
-                console.log("Error is:" + error)
-            }
-        }
-        fetchData();
-    }, [])
+  function DisplayMenClothing() {
+    const updatedList = originalData.filter((item) => {
+      return item.category === "men's clothing";
+    });
+    setData(updatedList);
+  }
+  function Displayjewelery() {
+    const updateList = originalData.filter((item) => {
+      return item.category === "jewelery";
+    });
 
-    function DisplayMenClothing() {
-        const updatedList = originalData.filter((item) => {
-            return (
-                item.category === "men's clothing"
-            )
-        })
-        setData(updatedList)
-    }
-    function Displayjewelery() {
-        const updateList = originalData.filter((item) => {
-            return (
-                item.category === "jewelery"
-            )
-        })
+    setData(updateList);
+  }
+  function Displayelectronics() {
+    const updateList = originalData.filter((item) => {
+      return item.category === "electronics";
+    });
+    setData(updateList);
+  }
+  function Dispalywomenclothing() {
+    const updateList = originalData.filter((item) => {
+      return item.category === "women's clothing";
+    });
+    setData(updateList);
+  }
 
-        setData(updateList);
+  function DisplayAll() {
+    setData(originalData);
+  }
 
-    }
-    function Displayelectronics() {
-        const updateList = originalData.filter((item) => {
-            return (
-                item.category === "electronics"
-            )
-        })
-        setData(updateList)
+  function DeleteItem(num: number) {
+    const deleteItem = async () => {
+      try {
+        const response = await axios.delete(
+          `https://fakestoreapi.com/products/${num}`
+        );
+        console.log(response.data);
+        setData(data.filter((item) => item.id !== num));
+        setOriginalData(originalData.filter((item) => item.id !== num));
+      } catch (error) {
+        console.log("Error message is" + error);
+      }
+    };
+    deleteItem();
+  }
 
-    }
-    function Dispalywomenclothing() {
-        const updateList = originalData.filter((item) => {
-            return (
-                item.category === "women's clothing"
-            )
-        })
-        setData(updateList);
+  function updateItem(num: number, itemPrice: number) {
+    const UpdateItem = async () => {
+      try {
+        const response = await axios.put(
+          `https://fakestoreapi.com/products/${num}`,
+          {
+            price: itemPrice + 1.0,
+          }
+        );
+        const newval = response.data;
+        console.log(response.data);
+        const NewList = data.map((item) =>
+          item.id === num ? { ...item, ...newval } : item
+        );
+        setData(NewList);
+        // setOriginalData(
+        //   originalData.map((item) =>
+        //     item.id === num ? { ...item, ...NewList } : item
+        //   )
+        // );
+        setOriginalData(NewList);
+      } catch (error) {
+        console.log("Error Message" + error);
+      }
+    };
+    UpdateItem();
+  }
 
-    }
-
-    function DisplayAll() {
-        setData(originalData);
-    }
-
-    function DeleteItem(num: number) {
-        const updateList = data.filter((item) => {
-            return (
-                item.id !== num
-            )
-        })
-        setData(updateList)
-    }
-
-    return (
-        <View style={styles.container}>
-            <FlatList
-                data={data}
-                keyExtractor={(item) => item.id.toString()}
-                renderItem={({ item }) => <DisplayMainItem items={item} getId={DeleteItem} />}
-                ListHeaderComponent={() => {
-                    return (
-                        <ScrollView horizontal={true} showsHorizontalScrollIndicator={false} >
-                            <View style={{ marginTop: 50, flexDirection: "row", justifyContent: "space-evenly", alignItems: "center", gap: 20 }}>
-                                <TouchableOpacity style={[styles.btnstyle, { width: 120 }]} onPress={DisplayAll}><Text>All</Text></TouchableOpacity>
-                                <TouchableOpacity style={[styles.btnstyle, { width: 120 }]} onPress={DisplayMenClothing}><Text>men's clothing</Text></TouchableOpacity>
-                                <TouchableOpacity style={[styles.btnstyle, { width: 120 }]} onPress={Displayjewelery}><Text>jewelery</Text></TouchableOpacity>
-                                <TouchableOpacity style={[styles.btnstyle, { width: 120 }]} onPress={Displayelectronics}><Text>electronics</Text></TouchableOpacity>
-                                <TouchableOpacity style={[styles.btnstyle, { width: 120 }]} onPress={Dispalywomenclothing}><Text>women's clothing</Text></TouchableOpacity>
-                            </View>
-                        </ScrollView>
-                    )
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={data}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => (
+          <DisplayMainItem
+            items={item}
+            getId={DeleteItem}
+            getVal={updateItem}
+          />
+        )}
+        ListHeaderComponent={() => {
+          return (
+            <ScrollView
+              horizontal={true}
+              showsHorizontalScrollIndicator={false}
+            >
+              <View
+                style={{
+                  marginTop: 50,
+                  flexDirection: "row",
+                  justifyContent: "space-evenly",
+                  alignItems: "center",
+                  gap: 20,
                 }}
-
-            />
-
-        </View >
-    )
+              >
+                <TouchableOpacity
+                  style={[styles.btnstyle, { width: 120 }]}
+                  onPress={DisplayAll}
+                >
+                  <Text>All</Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                  style={[styles.btnstyle, { width: 120 }]}
+                  onPress={DisplayMenClothing}
+                >
+                  <Text>men's clothing</Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                  style={[styles.btnstyle, { width: 120 }]}
+                  onPress={Displayjewelery}
+                >
+                  <Text>jewelery</Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                  style={[styles.btnstyle, { width: 120 }]}
+                  onPress={Displayelectronics}
+                >
+                  <Text>electronics</Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                  style={[styles.btnstyle, { width: 120 }]}
+                  onPress={Dispalywomenclothing}
+                >
+                  <Text>women's clothing</Text>
+                </TouchableOpacity>
+              </View>
+            </ScrollView>
+          );
+        }}
+      />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: "powderblue",
-        width: "100%"
-    },
-    listheaderssty: {
-
-    },
-    btnstyle: {
-
-        borderWidth: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        borderRadius: 15,
-        height: 35,
-        width: 70,
-    }
-
-})
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "powderblue",
+    width: "100%",
+  },
+  listheaderssty: {},
+  btnstyle: {
+    borderWidth: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 15,
+    height: 35,
+    width: 70,
+  },
+});
